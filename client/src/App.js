9ce0616adc1234b3d1ed78221138aa09aab5ddee
hahnrena <renabhahn@gmail.com>
2020-03-19 12:46:55 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HomePage from './components/homePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
