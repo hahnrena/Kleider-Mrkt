d73980f8858ece63ee5189266da9115dddd80c56
hahnrena <renabhahn@gmail.com>
2020-03-25 12:04:03 -0400

    Finished code for Directory & Shop Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homePage';
+import ShopPage from './pages/shopPage/shopPage';
 import './App.css';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- client/src/pages/homepage/homePage.js
+++ client/src/pages/homepage/homePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './homePage.styles.scss'
 import Directory from '../../components/directory/Directory';
 
+import './homePage.styles.scss'
 const HomePage = () => (
     <div className='homepage'>
         <Directory />
