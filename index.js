02c8e88a2d68a12c40a40ec64c58a088a3d1e6f6
hahnrena <renabhahn@gmail.com>
2020-04-06 10:58:20 -0400

    changed start scripts for deployment

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('client'));
+app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 
 
