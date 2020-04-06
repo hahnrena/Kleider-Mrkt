85fe25d6941256df46ae6fea933c140e92e22533
hahnrena <renabhahn@gmail.com>
2020-04-06 11:18:18 -0400

    npm run build

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const path = require('path');
 const express = require('express');
-
 const app = express();
 
-app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+    
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+});
 
 
 const PORT = process.env.PORT || 3000;  
