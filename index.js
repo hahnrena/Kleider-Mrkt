29672af6d9b7a85504d0763cb802a3cebc17f5e9
hahnrena <renabhahn@gmail.com>
2020-04-06 12:36:48 -0400

    created heroku env variables

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.static('client/build'));
     
 app.get('*', (req, res) => {
-    console.log(req, res)
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 });
 
