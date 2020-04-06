d5b47508f972224604907bcb93d4685de4eff408
hahnrena <renabhahn@gmail.com>
2020-04-06 11:49:50 -0400

    3rd attempt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('client/build'));
+app.use(express.static('/../client/build'));
     
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
