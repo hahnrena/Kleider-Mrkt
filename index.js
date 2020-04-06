d0448d7b26239d0c7753f89b96aa198db45b3656
hahnrena <renabhahn@gmail.com>
2020-04-06 09:15:06 -0400

    fixed for deployment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
+// app.use(express.static('client/build'));
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     });
