07fd53f6fcf6727d099c29c20e142789485b5224
hahnrena <renabhahn@gmail.com>
2020-04-06 12:03:28 -0400

    fixed index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('/../client/build'));
+app.use(express.static('client/build'));
     
 app.get('*', (req, res) => {
+    console.log(req, res)
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 });
 
 
-const PORT = process.env.PORT || 3000;  
+const PORT = process.env.PORT || 5000;  
 app.listen(PORT);
